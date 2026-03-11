7125e9ada59e3b2c1cb8d8377b25e3e3d234ec55
19nelly <nelly.kithinji@strathmore.edu>
2026-03-11 13:30:27 +0300

    11/03/2026 CLASS WORK

---
+++ flutter_application_1/lib/views/homescreen.dart
@@ -0,0 +1,34 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/configs/colors.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 15, 5, 147),
+        color: primaryColor,
+
+        buttonBackgroundColor: secondaryColor,
+        items: <Widget>[
+          Icon(Icons.dashboard, size: 30, color: Colors.white),
+          Icon(Icons.category, size: 30),
+          Icon(Icons.list, size: 30),
+          Icon(Icons.person, size: 30),
+        ],
+        onTap: (index) {
+          // Handle button tap
+        },
+      ),
+      body: Container(), // Add your main content here
+    );
+  }
+}
